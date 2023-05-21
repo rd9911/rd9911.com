29afc3645da7bfcb95825f0c120814e3f04f3e0b
Avazjon Yusufjonov <68438307+rd9911@users.noreply.github.com>
2023-05-21 15:28:13 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,6 @@ export default function Home() {
                 <div className='social-links'>
                   <a className='link' type='button' href='https://github.com/rd9911' target='_blank' rel='noreferrer' ><Image src={github_image} alt='github-logo' width={58} height={30} /></a>
                   <a className='link' type='button' href='https://www.linkedin.com/in/avazjonyusufjonov1/' target='_blank' rel='noreferrer' ><Image src={linkedin_image} alt='linkedin-logo' width={30} height={30} /></a>
-                  <a className='link' type='button' href='https://twitter.com/brodsky9911' target='_blank' rel='noreferrer' ><Image src={twitter_image}  alt='twitter-logo' width={35} height={35} /></a>
-                  <a className='link' type='button' href='https://t.me/rd9911' target='_blank' rel='noreferrer' ><Image src={telegram_image} alt='telegram-logo' width={50} height={28} /></a>
                   <a className='link' type='button' href='mailto:avazjonyusufjonov@gmail.com' target='_blank' rel='noreferrer' ><Image src={mail_image} alt='mail-logo' width={40} height={30} /></a>
                 </div>
               </div>
